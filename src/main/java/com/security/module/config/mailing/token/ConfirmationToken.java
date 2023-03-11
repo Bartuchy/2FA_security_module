4a8591f3b95c8737283f6b6415111af24a1cb5ca
Bartek <bartekmark33@interia.pl>
2023-03-11 23:02:36 +0100

    created confirmation token layers

---
+++ src/main/java/com/security/module/config/mailing/token/ConfirmationToken.java
@@ -0,0 +1,33 @@
+package com.security.module.config.mailing.token;
+
+import com.security.module.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class ConfirmationToken {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    private LocalDateTime confirmedAt;
+
+    @ManyToOne
+    private User user;
+}
